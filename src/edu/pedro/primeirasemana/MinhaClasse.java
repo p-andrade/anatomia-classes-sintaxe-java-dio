6ed04575f9d61198623015b4b2977c01b8373daa
p-andrade <pedrotmajr@gmail.com>
2022-11-12 20:37:11 -0300

    Repositório sobre estudo da Anatomia das Classes do Java

---
+++ src/edu/pedro/primeirasemana/MinhaClasse.java
@@ -0,0 +1,31 @@
+package edu.pedro.primeirasemana;
+
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        //System.out.print("Olá turma, sejam bem-vindos");
+            
+            //String primeiroNome = "Pedro ";
+            //String segundoNome = "Andrade";
+
+            //String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+            //System.out.println(nomeCompleto);
+            //String meuNome = "Pedro";
+            //int anoFrabricacao = 2022;
+            //boolean verdadeira = true;
+            //anoFrabricacao = 2018;
+            
+            //int ano = 2021;
+            //ano = 2022;
+
+            //final String BR = "Brasil";
+            //BR = "Brazil";
+
+    }
+    
+    //public static String nomeCompleto (String primeiroNome, String segundoNome){
+     //   return "Resultado do método " + primeiroNome.concat("").concat(segundoNome);
+    //}
+    
+    
+}
